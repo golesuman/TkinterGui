aeaf4ea072de88b7c0b69004acfb9f52c9658df5
Suman Gole <60792767+golesuman@users.noreply.github.com>
2020-11-11 10:16:41 +0545

    Add files via upload

---
+++ Upload photo on GUI/tkinter Tutorial.py
@@ -0,0 +1,11 @@
+from tkinter import *
+root=Tk()
+#width and height
+root.geometry('644x434')
+#width, height
+root.minsize(200,100)
+#width , height
+# root.maxsize(200,100)
+myInfo=Label(text='Hi this is the basic Interface')
+myInfo.pack()
+root.mainloop()
---
+++ Upload photo on GUI/tkinterfile.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root=Tk()
+root.geometry('600x450')
+photo=PhotoImage(file='su.png')
+mywallpaper=Label(image=photo)
+mywallpaper.pack()
+# mylabel=Label(root, text='My Name is Suman Gole')
+# mylabel.grid(row=0, column=0)
+mainloop()
